a66013d608c1ea583c06d41acf1cadd1c85ab54b
Junaidi Rahmat <junaidirahmat02@gmail.com>
2025-01-31 13:24:37 +0700

    feat : initialize deploy

--- src/feature/kalibrasi_co2/repository/co2.ts
+++ src/feature/kalibrasi_co2/repository/co2.ts
@@ -1,4 +1,4 @@
-import prisma from "../../../../config/database";
+import prisma from "../../../config/database";
 import { Co2 } from "@prisma/client";
 
 class Co2Repository {
--- src/feature/kalibrasi_pm25/repository/pm25.ts
+++ src/feature/kalibrasi_pm25/repository/pm25.ts
@@ -1,4 +1,4 @@
-import prisma from "../../../../config/database";
+import prisma from "../../../config/database";
 import { Pm25Data } from "@prisma/client";
 
 class Pm25Repository {
